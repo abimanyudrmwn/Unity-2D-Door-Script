60f06681c5bb3950e3613c814447af21e9121c26
Abimanyu Darmawan <48431686+abimanyudrmwn@users.noreply.github.com>
2020-11-17 16:55:25 +0700

    Add SceneDoorChange.cs
    Merupakan file script Perpindahan Scene menggunakan collider 2d

---
+++ SceneDoorChange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneDoorChange : MonoBehaviour
+{
+    public int LevelToLoad; // Var Integer untuk Load Level 1 pada Game
+	private int currentSceneIndex; // Untuk mengetahui Index Scene
+	
+	void Start()
+	{
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D col) // Ketika Player Menyentuh Collider 2D Dalam Game
+	{
+		if (col.CompareTag("Player")) // Handler untuk mengetahui Collider Player
+		{
+				currentSceneIndex = SceneManager.GetActiveScene().buildIndex; // Detect Scene yang ada
+				PlayerPrefs.SetInt("SavedScene", currentSceneIndex); // Scene di Save
+				SceneManager.LoadScene(LevelToLoad); // Lalu Scene akan berpindah ke Level 1
+		}
+	}	
+	
+}
